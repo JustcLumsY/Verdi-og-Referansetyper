1bbd8823248dd423dd7f809027b8817b884bd9a5
John Espen Thoresen <84008816+JustcLumsY@users.noreply.github.com>
2022-01-05 10:36:39 +0100

    Add project files.

---
+++ Verdi og Referansetyper/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Verdi_og_Referansetyper
+{
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            var a = 5;
+            var b = 3.0;
+            var sum = a + b;
+            Console.WriteLine($"A + B = SUM {a} + {b} = {sum}");
+            //bruker ${} i tekst string for variabler(var {a}).
+        }
+    }
+}
+
+
+//int tall = 1;
+
+//float desimalTall = 1.01f; //Nøyaktig
+//double desimalTall2 = 1.01;
+//decimal desimalTall3 = 1.01M; // Veldig nøyaktig
+
+//char bokstav = 'a';
+//string tekst = "hei";
+
+//bool santUsant = true; // Eller false
+
+//int[] tallArray = { 1, 2, 3 };
+//string[] tekstArray = { "hei", "på", "deg" };
+//List<int> liste = new List<int>();
